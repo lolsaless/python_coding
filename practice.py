2ce579d35f108df9ec503e5f638ea790712805d0
jouiho <hoof.jo@gmail.com>
2021-06-29 23:01:54 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,14 @@
+station1 = '사당'
+station2 = '신도림'
+station3 = '인천공항'
+
+print(station1 + '행 열차가 들어오고 있습니다.')
+
+print(3-2)
+print(5*3)
+
+print(1 != 3) #True
+print(not(1 != 3)) #False
+
+print((3>0) and (3 < 5)) #True
+print((3 > 0 ) & (3 < 5))
