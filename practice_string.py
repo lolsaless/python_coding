a123df3bda1fd143a6bfcb8fe8ddab64573b2317
lolsaless <hoof.jo@gmail.com>
2021-07-02 18:43:02 +0900

    l

--- practice_string.py
+++ practice_string.py
@@ -1,34 +1,62 @@
-sentence = '나는 소년입니다.'
-sentence2 = "파이썬은 쉽습니다."
-sentence3 = """
-나는 소년이고,
-파이썬은 쉽습니다.
-"""
+# sentence = '나는 소년입니다.'
+# sentence2 = "파이썬은 쉽습니다."
+# sentence3 = """
+# 나는 소년이고,
+# 파이썬은 쉽습니다.
+# """
 
-print(sentence3)
+# print(sentence3)
 
 
-#데이터 슬라이싱
-number = "860101-1143419"
-print("성별:" + number[7])
-print(number.split('-')[0][1])
+# #데이터 슬라이싱
+# number = "860101-1143419"
+# print("성별:" + number[7])
+# print(number.split('-')[0][1])
 
-python = "Python is Amazing"
-print(python.lower())
-print(python.upper())
-print(not(python[0].isupper()))
-print(len(python))
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(not(python[0].isupper()))
+# print(len(python))
 
-print(python.replace("Python", "Java"))
+# print(python.replace("Python", "Java"))
 
-index = python.index("n")
+# index = python.index("n")
 
-print(index)
+# print(index)
 
-index = python.index("n", index + 1)
-print(index)
+# index = python.index("n", index + 1)
+# print(index)
 
-print(python.find("n"))
-print(python.find("n",6))
+# print(python.find("n"))
+# print(python.find("n",6))
 
-print(python.count("n"))
+# print(python.count("n"))
+
+
+# print('a' + 'b')
+
+# print("나는 %d살입니다." %20)
+# print("나는 %s을 좋아해요" %'파이썬')
+
+# print("나는 {}살입니다.".format(20))
+# print("나는 {}색과 {}색을 좋아합니다.".format('파란', '빨간'))
+# print("나는 {1}색과 {0}색을 좋아합니다.".format('파란', '빨간'))
+
+
+# print('나는 {age}살이며, {color}색을 좋아합니다.'.format(age=20, color="빨강"))
+
+age = 20
+color = "빨강"
+print(f"나는 {age}살이며, {color}색을 좋아합니다 ")
+
+#\n 줄바꿈
+print("백문이 불여일견,\n백견이 불여일타")
+
+print('저는 "나도코딩" 입니다.')
+
+print("저는 \"나도코딩\"입니다.")
+
+#\\ : 문장 내에서는 \로 나타남
+
+print("")
