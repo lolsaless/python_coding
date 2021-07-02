007c4eb34b3ca5b7b9487b3afb2b2a92e1a8bc0c
jouiho <hoof.jo@gmail.com>
2021-07-02 19:15:08 +0900

    ㅎ

--- practice_string.py
+++ practice_string.py
@@ -46,17 +46,51 @@
 
 # print('나는 {age}살이며, {color}색을 좋아합니다.'.format(age=20, color="빨강"))
 
-age = 20
-color = "빨강"
-print(f"나는 {age}살이며, {color}색을 좋아합니다 ")
+# age = 20
+# color = "빨강"
+# print(f"나는 {age}살이며, {color}색을 좋아합니다 ")
 
-#\n 줄바꿈
-print("백문이 불여일견,\n백견이 불여일타")
+# #\n 줄바꿈
+# print("백문이 불여일견,\n백견이 불여일타")
 
-print('저는 "나도코딩" 입니다.')
+# print('저는 "나도코딩" 입니다.')
 
-print("저는 \"나도코딩\"입니다.")
+# print("저는 \"나도코딩\"입니다.")
 
-#\\ : 문장 내에서는 \로 나타남
+# #\\ : 문장 내에서는 \로 나타남
 
-print("")
+# #\r : 커서를 맨 앞으로 이동
+# print("red white apple\rPine")
+
+# #\b : 백스페이스(한 글자 삭제)
+# print("redd\bApple")
+
+# #\t : tab
+# print('red\tapple\nwhite')
+
+
+#Quiz
+http = "http://naver.com"
+# http = "http://daum.net"
+# http_split = http.split('//')
+# print(http_split[1])
+# http_naver = http_split[1]
+# http_naver_split = http_naver.split('.')
+# naver = http_naver_split[0]
+# print(naver)
+
+# password = naver[:3] + str(len(naver)) + str(naver.count('e')) + "!"
+# print(password)
+
+# my_str = http.replace('http://', '')
+# my_str = my_str[:my_str.index(".")]
+# password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+
+# print("{0}의 비밀번호는 {1}입니다.".format(http, password))
+
+
+
+my_str = http.replace('http://', '')
+my_str = my_str[:my_str.index(".")]
+password = my_str[:3] + str(len(my_str)) + str(my_str.count('e')) + '!'
+print(f"{http}의 비밀번호는 {password}입니다.")
