aece26baa60ff3ad4699ac741c395f4104364bda
jouiho <hoof.jo@gmail.com>
2021-07-05 23:49:33 +0900

    210705

---
+++ practice_자료구조.py
@@ -0,0 +1,23 @@
+# menu = {"coffe", "milk", "juice"}
+# print(menu, type(menu))
+
+# menu = list(menu)
+# print(menu, type(menu))
+
+# menu = tuple(menu)
+# print(menu, type(menu))
+
+# menu = set(menu)
+# print(menu, type(menu))
+
+
+# Quiz
+
+from random import *
+
+lst = [1,2,3,4,5]
+shuffle(lst)
+lst_sample = sample(lst, 1)
+print(lst_sample)
+
+lst
