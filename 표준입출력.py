2665d7da0e65057b8591215c43221594ff190adc
jouiho <hoof.jo@gmail.com>
2021-07-13 20:58:59 +0900

    0713

--- 표준입출력.py
+++ 표준입출력.py
@@ -1,1 +1,10 @@
-print("Python", "Java", sep=" vs ")
+# import sys
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
+
+scores = {"수학" : 0, "영어" : 50, "코딩" : 100}
+
+for subjects, score in scores.items():
+    print(subjects.ljust(3), str(score).rjust(4), sep=":")
+    
+
