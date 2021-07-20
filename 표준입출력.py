b430ffd9bf89fc1dd69fc9c5cbcb0308bfc5756f
jouiho <hoof.jo@gmail.com>
2021-07-20 21:47:41 +0900

    20210720

--- 표준입출력.py
+++ 표준입출력.py
@@ -2,9 +2,15 @@
 # print("Python", "Java", file=sys.stdout)
 # print("Python", "Java", file=sys.stderr)
 
-scores = {"수학" : 0, "영어" : 50, "코딩" : 100}
+# scores = {"수학" : 0, "영어" : 50, "코딩" : 100}
 
-for subjects, score in scores.items():
-    print(subjects.ljust(3), str(score).rjust(4), sep=":")
-    
+# for subjects, score in scores.items():
+#     print(subjects.ljust(3), str(score).rjust(3), sep=":")
 
+# 은행 대기순번표 001, 002, 003  
+# for num in range(1,21):
+#     print("대기번호 : " + str(num).zfill(4))
+
+answer = input("아무 값이나 입력하세요 : ")
+
+print("입력하신 값은 " + answer + "입니다.")
